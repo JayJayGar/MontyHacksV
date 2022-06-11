8b2157f6b9ebc2d527d218d5977f1c552c385374
Jack <jay.jay.gar@comcast.net>
2022-06-11 12:57:25 -0400

    Highlights working

--- MontyHacksV/Assets/Scripts/GridManager.cs
+++ MontyHacksV/Assets/Scripts/GridManager.cs
@@ -10,6 +10,8 @@ public class GridManager : MonoBehaviour
 
     [SerializeField] private Transform _cam;
 
+    private Dictionary<Vector2, Tile> _tiles;
+
     private void Start()
     {
         GenerateGrid();
@@ -17,6 +19,7 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        _tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
@@ -24,8 +27,7 @@ public class GridManager : MonoBehaviour
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
-                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
-                spawnedTile.Init(isOffset);
+                _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
 
