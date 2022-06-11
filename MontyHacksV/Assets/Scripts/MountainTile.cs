70a1ead0788bab29326e37252d7d24c4d128bc18
Bumbo333 <jeffrey.chan178@gmail.com>
2022-06-11 14:21:33 -0400

    Revert "uh"
    This reverts commit fd05cf5e0e0ea1436a629de04a18a1f4015f2102.

--- MontyHacksV/Assets/Scripts/MountainTile.cs
+++ MontyHacksV/Assets/Scripts/MountainTile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MountainTile : Tile
+public class MountainTile : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
