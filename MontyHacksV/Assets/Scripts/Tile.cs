e7b1899469daf71d76897cbe9388e4348269df68
Gibbersnitch <gabriel_makris@hotmail.com>
2022-06-13 00:03:21 -0400

    Good Offset
    Ok

--- MontyHacksV/Assets/Scripts/Tile.cs
+++ MontyHacksV/Assets/Scripts/Tile.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField] private Color _baseColor, _offsetColor;
+    [SerializeField] private Color _baseColor;
     [SerializeField] private SpriteRenderer _sprite;
     [SerializeField] private GameObject _highlight;
 
     public void Init(bool isOffset){
-        _sprite.color = isOffset ? _offsetColor : _baseColor;
         
+        _sprite.color = isOffset ? new Color(0.6f,0.6f,0.6f,1f) : new Color(255f,255f,255f,1f);
     }
 
     private void OnMouseEnter() {
